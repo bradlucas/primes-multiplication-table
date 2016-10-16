85e10a756fedf78bdb6af237f72bbfa115f07e7a
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:18:15 -0400

    version 1.8.0 of clojure

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject primes-multiplication-table "0.1.0-SNAPSHOT"
+(defproject primes-multiplication-table "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main primes-multiplication-table.core
 )
